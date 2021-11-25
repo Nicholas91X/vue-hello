646d29386c5db6a0015346c6b880ecfb66f4587c
Nicholas <romitinicholas@gmail.com>
2021-11-25 16:33:30 +0100

    vue hello

---
+++ script.js
@@ -0,0 +1,13 @@
+// Descrizione:
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data
+
+// Bonus:
+// Aggiungere alla pagina un'immagine, presa anch'essa da un data
+
+const app = new Vue({
+    el: "#root",
+    data: {
+        messaggio: "Hello World!",
+        immagine: "https://images.unsplash.com/photo-1637780276724-e20009e9f5ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60"
+    }
+})
